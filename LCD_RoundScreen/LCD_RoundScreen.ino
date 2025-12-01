39ca72a41c9e040f3ca66cf7f27d6a8640d63c71
RowansWackoStuff <rowan.wildman@outlook.com>
2025-12-01 11:57:26 +1100

    swapped out the c3 for a c6 to load for sand particles (script pins updated to suit the board))

--- LCD_RoundScreen/LCD_RoundScreen.ino
+++ LCD_RoundScreen/LCD_RoundScreen.ino
@@ -9,10 +9,15 @@
 Adafruit_MPU6050 mpu;
  
 #define TFT_DC 2
-#define TFT_CS 7
+#define TFT_CS 4
+#define TFT_RST 5
+
+#define TFT_SCK 18
+#define TFT_MOSI 19
+#define TFT_MISO 20
  
 // Hardware SPI on Feather or other boards
-Adafruit_GC9A01A tft(TFT_CS, TFT_DC);
+Adafruit_GC9A01A tft(TFT_CS, TFT_DC, TFT_RST);
 
 float averageRoll = 0;
 float averagePitch = 0;
@@ -28,104 +33,96 @@ void setup() {
 
   Serial.begin(9600);
 
+  SPI.begin(TFT_SCK, TFT_MISO, TFT_MOSI);
+
   Wire.begin(1, 3); // Specify SDA (0) and SCL (5) for ESP32-C3
 
+  Serial.println("1");
+
+  // test mpu innitalisation is functioning
   if (!mpu.begin()) {
     Serial.println("MPU6050 connection failed!");
     while (1);
   }
 
-  
-  Serial.println("GC9A01A Test!");
- 
+  Serial.println("2");
+
+  // establish TFT communication
   tft.begin();
 
-  sensors_event_t a, g, temp;
-  
-  for(int i = 0; i < 25; i++){
-    mpu.getEvent(&a, &g, &temp);
-    float roll = atan2(a.acceleration.y, a.acceleration.z) * 180 / PI;
-    float pitch = atan2(-a.acceleration.x, sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180 / PI;
-
-    averageRoll += roll;
-    averagePitch += pitch;
-
-    if(pitch < minPitch){
-      minPitch = pitch;
-    }
-
-    if(pitch > maxPitch){
-      maxPitch = pitch;
-    }
-
-    if(roll < minRoll){
-      minRoll = roll;
-    }
-
-    if(roll > maxRoll){
-      maxRoll = roll;
-    }
-  }
-
-  averageRoll /= 25;
-  averagePitch /= 25;
-
-  Serial.println(averageRoll);
-  Serial.println(averagePitch);
-
+  // reset the screen
   tft.fillScreen(GC9A01A_BLACK);
 
   initialiseSand();
+
+  Serial.println("3");
 }
-
-int ballY = 120;
-int ballX = 120;
-
-// can do distance from the center to stop from going outta bounds
- 
 void loop(void) {
-  // ball();
   sand();
+  // ball();
 
 }
 
-const int sandAmount = 900;
+const int sandAmount = 4000;
 int sandX[sandAmount];
 int sandY[sandAmount];
 
 int sandGrid[240][240];
 
 void initialiseSand(){
+  // create sand particles
   for(int i = 0; i < sandAmount; i++){
-    sandX[i] = random(0, 240);
-    sandY[i] = random(0, 240);
+    bool avaliable = false;
 
+    // ensure that sand isnt placed ontop of eachother
+    while(avaliable == false){
+      sandX[i] = random(0, 240);
+      sandY[i] = random(0, 240);
+
+      if(sandGrid[sandX[i]][sandY[i]] == 0){
+        sandGrid[sandX[i]][sandY[i]] = 1;
+        avaliable = true;
+      }
+    }
+    // draw particle onto the screen
     tft.drawPixel(sandX[i], sandY[i], GC9A01A_RED);
-
-    sandGrid[sandX[i]][sandY[i]] = 1;
   }
-
-
 }
 
+// !!! TODO
+// sand collsion is causing the sand to fall slowly (maybe?)
+// sand also keeps getting stuck
+
+// change the sensativity to 0 (done)
+// create better collision
+// remove drifting
+
 void sand(){
+
+  // creates 3 varaibles to hold the acceleration, gyroscope and tempreture data
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  // --- 1. Basic Accelerometer-based Roll/Pitch ---
-  // (In a real application, you would use a Sensor Fusion algorithm here)
+  
+  // get roll and pitch of the gyroscope
+  // EXPLAIN HOW IT WORKS
   float roll = atan2(a.acceleration.y, a.acceleration.z) * 180 / PI;
   float pitch = atan2(-a.acceleration.x, sqrt(a.acceleration.y * a.acceleration.y + a.acceleration.z * a.acceleration.z)) * 180 / PI;
 
+  Serial.println("roll: ");
+  Serial.print(roll);
+  Serial.print(" pitch: ");
+  Serial.print(pitch);
+
+  // move all sand particles
   for(int i = 0; i < sandAmount; i++){
 
+    // remove prior sand particle
     tft.drawPixel(sandX[i], sandY[i], GC9A01A_BLACK);
     sandGrid[sandX[i]][sandY[i]] = 0;
 
-    int newX = sandX[i] + round(roll * 0.1);
-    int newY = sandY[i] - round(pitch * 0.1);
-
-    // sandX[i] += round(roll * 0.1);
-    // sandY[i] -= round(pitch * 0.1);
+    // get new sand particle position
+    int newX = sandX[i] + round(roll * 1); // 0.1
+    int newY = sandY[i] - round(pitch * 1); // 0.1
 
     // keep sand in screen
     int centerX = 120;
@@ -142,38 +139,25 @@ void sand(){
         newY = centerY + dy * scale;
     }
 
+    // check if sand is colliding
     bool collide = false;
-
-    // only check if the spots around it are full?
-
-    // for(int j = 0; j < sandAmount; j++){
-    //   if(newX == sandX[j] && newY == sandY[j]){
-    //     collide = true;
-    //     break;
-    //   }
-    // }
-
-    if(sandGrid[newX][newY] == 1){
-      collide = true;
-    }
-
-    if(collide == false){
+    if(sandGrid[newX][newY] == 0){
       sandX[i] = newX;
       sandY[i] = newY;
     }
-    sandGrid[sandX[i]][sandY[i]] = 1;
 
+    // draw sand particle in new position
+    sandGrid[sandX[i]][sandY[i]] = 1;
     tft.drawPixel(sandX[i], sandY[i], GC9A01A_RED);
 
     
   }
 
   // delay(10);
-
-  // update position
-  // ballX += round(roll * 0.1);
-  // ballY -= round(pitch * 0.1);
 }
+
+int ballX = 120;
+int ballY = 120;
 
 void ball(){
   sensors_event_t a, g, temp;
